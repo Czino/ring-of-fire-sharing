0c6408d7aa3ecaf593e8618b049963ca3265582f
Czino <eric@czino.de>
2021-04-29 13:32:21 +0200

    Add convenience method for adding new peers

---
+++ addPeer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )/."
+
+config_file=ringOfFireConfig.json
+if [ ! -f "$config_file" ]; then
+  echo "$config_file does not exists."
+  return
+fi
+
+read -p 'Node id of new peer: ' peer
+
+config=$(jq --arg peer "$peer" '.peers += [$peer]' "$config_file")
+echo "$config" | tee "$config_file" >/dev/null
