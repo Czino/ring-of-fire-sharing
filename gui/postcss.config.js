7b9c61fe535794617393cb003a774f7a06f67a7e
Czino <eric@czino.de>
2021-05-13 15:44:04 +0200

    Purge CSS

--- gui/postcss.config.js
+++ gui/postcss.config.js
@@ -3,15 +3,22 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 module.exports = {
   plugins: [
     'tailwindcss',
-    // purgecss({
-    //   preserveHtmlElements: false,
-    //   content: [
-    //     './dist/**/*.html',
-    //     './dist/**/*.js',
-    //   ],
-    //   safelist: [
-    //     /\//,
-    //   ]
-    // })
+    purgecss({
+      preserveHtmlElements: false,
+      content: [
+        './src/**/*.html',
+        './src/**/*.js',
+      ],
+      defaultExtractor: content => {
+        // Capture as liberally as possible, including things like `h-(screen-1.5)`
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+        const broadMatchesWithoutTrailingSlash = broadMatches.map(match => match.replace(/\/$/, ''))
+
+        // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+        const innerMatches = content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || []
+
+        return broadMatches.concat(broadMatchesWithoutTrailingSlash).concat(innerMatches)
+      }
+    })
   ]
 }
